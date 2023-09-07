c1013382709b0a383006bb2162535f4286f89071
mohammed herzallah <herzallahmkh1620@gmail.com>
2023-09-07 08:04:12 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdiostream>
+/**
+ * main - A program that prints a line with puts functio
+n
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
